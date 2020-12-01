c0010b45a7808428ddd925065f5b3810a9b1c29c
JanAlexanderZak <janalexzak@gmail.com>
2020-12-01 09:35:45 +0100

    add: expected_loss

---
+++ tests/finpy/test_expected_loss.py
@@ -0,0 +1,9 @@
+import pytest
+from finpy.expected_loss import expected_loss
+
+
+@pytest.mark.parametrize(
+    'prob_default, exposure_default, loss_default, expected', [
+        (0.04, 725, 0.25, 21.75), ])
+def test_expected_loss(prob_default, exposure_default, loss_default, expected):
+    assert expected_loss(prob_default, exposure_default, loss_default) == expected
