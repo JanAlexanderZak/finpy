dd1c66e07b20df51868d6d7373410a2961054cb2
JanAlexanderZak <janalexzak@gmail.com>
2020-12-08 17:42:05 +0100

    refactor: merge with script: weighted_average_cost_of_capital.py

--- tests/finpy/test_discounting.py
+++ tests/finpy/test_discounting.py
@@ -18,3 +18,17 @@ class TestDiscounting:
     def test_year(self):
         discount = Discounting()
         assert discount.year(100, 0.0717, 50) == pytest.approx(10.009, 3)
+
+    @pytest.mark.parametrize(
+        'series_of_payments, wacc, _present_value', [
+            ([100, 80, 60, 150], 0.05, 343.03608),
+            ([100, 80, 60, 150], 0.07, 326.7452),
+        ]
+    )
+    def test_present_value_wacc(self, series_of_payments, wacc, _present_value):
+        discount = Discounting()
+        assert discount.present_value_wacc(series_of_payments, wacc) == pytest.approx(_present_value, 0.001)
+
+    def test_wacc(self):
+        discount = Discounting()
+        assert discount.wacc(0.1, 0.9, 0.1, 0.08, 0.3) == pytest.approx(0.0604, 0.001)
