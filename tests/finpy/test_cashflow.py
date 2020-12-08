a6c35aecdfe57cb1c327f754f4a3c63dbea8378f
JanAlexanderZak <janalexzak@gmail.com>
2020-12-08 18:44:07 +0100

    refactor: shortened fixture name

--- tests/finpy/test_cashflow.py
+++ tests/finpy/test_cashflow.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import pytest
+
 from finpy.cashflow import Cashflow
 
 
 class TestCashflow:
     @pytest.fixture
-    def company_debt_history_df(self):
+    def df_cf(self):
         # https://datatest.readthedocs.io/en/stable/how-to/fixtures/dataframe-fixture.html
         df = pd.DataFrame(
             [['Company A', '2000', 50, 10, 30, 10],
@@ -20,18 +21,18 @@ class TestCashflow:
             (100, 50, 80, 40, (20, 30)),
             (-100, 50, 80, 40, (-180, -170)),
         ], )
-    def test_cashflow_and_profit(self, cash_profit, non_cash_profit, cash_expense, non_cash_expense, expected, company_debt_history_df):
-        _, cf = company_debt_history_df
+    def test_cashflow_and_profit(self, cash_profit, non_cash_profit, cash_expense, non_cash_expense, expected, df_cf):
+        _, cf = df_cf
         assert cf.cashflow_and_profit(cash_profit, non_cash_profit, cash_expense, non_cash_expense) == expected
 
-    def test_debt_capacity(self, company_debt_history_df):
-        df, cf = company_debt_history_df
+    def test_debt_capacity(self, df_cf):
+        df, cf = df_cf
         assert cf.debt_capacity() == pytest.approx(286.6666, 3)
 
-    def test_free_cashflow(self, company_debt_history_df):
-        df, cf = company_debt_history_df
+    def test_free_cashflow(self, df_cf):
+        df, cf = df_cf
         assert cf.free_cashflow() == pytest.approx(43.3333, 3)
 
-    def test_average_cashflow(self, company_debt_history_df):
-        df, cf = company_debt_history_df
+    def test_average_cashflow(self, df_cf):
+        df, cf = df_cf
         assert cf.average_cashflow() == pytest.approx(73.3333, 3)
