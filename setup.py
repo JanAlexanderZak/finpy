3a4694cae22d8aa6e6d1c81f345c07fd50cad4c9
JanAlexanderZak <janalexzak@gmail.com>
2020-12-08 18:03:21 +0100

    add: setup.py for travis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pycharm',
+    description='Finpy',
+    author='Jan Alexander Zak',
+    author_email='?@gmail.com',
+    package_dir={'': 'finpy'},
+)
