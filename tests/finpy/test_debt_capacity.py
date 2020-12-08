a693e8cbf77b7ee2777ffc5ad0e6db0e7940e74e
JanAlexanderZak <janalexzak@gmail.com>
2020-12-08 17:26:59 +0100

    refactor: merged with script: 'cashflow_and_profit.py' and created class

--- tests/finpy/test_debt_capacity.py
+++ tests/finpy/test_debt_capacity.py
@@ -1,28 +1,37 @@
 import pandas as pd
 import pytest
-from finpy.debt_capacity import debt_capacity, free_cashflow, average_cashflow
+from finpy.debt_capacity import Cashflow
 
 
-@pytest.fixture
-def company_debt_history_df():
-    # https://datatest.readthedocs.io/en/stable/how-to/fixtures/dataframe-fixture.html
-    return pd.DataFrame(
-        [['Company A', '2000', 50, 10, 30, 10],
-         ['Company A', '2001', 55, 20, 29, 20],
-         ['Company A', '2002', 65, 20, 31, 20], ],
-        columns=['Company', 'Year', 'Profit', 'Depreciation', 'Running Cost', 'Debt'], )
+class TestCashflow:
+    @pytest.fixture
+    def company_debt_history_df(self):
+        # https://datatest.readthedocs.io/en/stable/how-to/fixtures/dataframe-fixture.html
+        df = pd.DataFrame(
+            [['Company A', '2000', 50, 10, 30, 10],
+             ['Company A', '2001', 55, 20, 29, 20],
+             ['Company A', '2002', 65, 20, 31, 20], ],
+            columns=['Company', 'Year', 'Profit', 'Depreciation', 'Running Cost', 'Debt'], )
+        cf = Cashflow(df['Profit'], df['Depreciation'], df['Running Cost'], df['Debt'])
+        return df, cf
 
+    @pytest.mark.parametrize(
+        'cash_profit, non_cash_profit, cash_expense, non_cash_expense, expected', [
+            (100, 50, 80, 40, (20, 30)),
+            (-100, 50, 80, 40, (-180, -170)),
+        ], )
+    def test_cashflow_and_profit(self, cash_profit, non_cash_profit, cash_expense, non_cash_expense, expected, company_debt_history_df):
+        _, cf = company_debt_history_df
+        assert cf.cashflow_and_profit(cash_profit, non_cash_profit, cash_expense, non_cash_expense) == expected
 
-def test_debt_capacity(company_debt_history_df):
-    df = company_debt_history_df
-    assert debt_capacity(df['Profit'], df['Depreciation'], df['Running Cost'], df['Debt']) == pytest.approx(286.6666, 3)
+    def test_debt_capacity(self, company_debt_history_df):
+        df, cf = company_debt_history_df
+        assert cf.debt_capacity() == pytest.approx(286.6666, 3)
 
+    def test_free_cashflow(self, company_debt_history_df):
+        df, cf = company_debt_history_df
+        assert cf.free_cashflow() == pytest.approx(43.3333, 3)
 
-def test_free_cashflow(company_debt_history_df):
-    df = company_debt_history_df
-    assert free_cashflow(df['Profit'], df['Depreciation'], df['Running Cost']) == pytest.approx(43.3333, 3)
-
-
-def test_average_cashflow(company_debt_history_df):
-    df = company_debt_history_df
-    assert average_cashflow(df['Profit'], df['Depreciation']) == pytest.approx(73.3333, 3)
+    def test_average_cashflow(self, company_debt_history_df):
+        df, cf = company_debt_history_df
+        assert cf.average_cashflow() == pytest.approx(73.3333, 3)
